2ca476bb371838360dffe2a88c9db381bd7980f6
Oliver Lucido <dev@redboxresearchdata.com.au>
2011-04-12 05:28:11 +0000

    added unix shell scripts

---
+++ harvester/harvest.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+usage() {
+    echo "This script requires the first parameter to be in the input file to ingest."
+    echo "NOTE: This input file is expected to be a tab delimited geonames data dump."
+    echo "Usage: `basename $0` inputFile.txt"
+	exit 1
+}
+
+# check script arguments
+[ $# -gt 0 ] || usage
+
+# get absolute path of where the script is run from
+PROGRAM_DIR=`cd \`dirname $0\`; pwd`
+
+if [ -f $1 ]; then
+	INPUT_FILE=$1
+    shift
+    MAVEN_ARGS="$*"
+    SOLR_HOME="-Dgeonames.solr.home=$PROGRAM_DIR/solr"
+    export MAVEN_OPTS="-XX:MaxPermSize=512m -Xmx1024m"
+	mvn $MAVEN_ARGS $SOLR_HOME \
+		-Dexec.args="$PROGRAM_DIR/$INPUT_FILE" \
+		-Dexec.mainClass=au.edu.usq.adfi.geonames.harvester.Harvester \
+		exec:java
+fi
